b18d851ef4e22cb884f334faa6c4c239643519e1
Brijesh <brijesh@differenz.co.in>
2018-04-27 13:43:15 +0530

    updating demo app version 1

---
+++ DemoBuddyApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DemoBuddyApp
+//
+//  Created by Administrator on 4/27/18.
+//  Copyright © 2018 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
